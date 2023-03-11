9dc18c14edb46c42e9072842688881c8c175e3d6
Shtrifel <bs.zubov@yandex.ru>
2023-03-11 11:49:25 +0300

    no message

---
+++ setupProject/ImagesList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  setupProject
+//
+//  Created by Иван Иванов on 11.03.2023.
+//
+
+import UIKit
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+
+}
