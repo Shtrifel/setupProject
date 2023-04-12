90d2db9529945d0372303cfe0051f928a7cbf16b
Shtrifel <bs.zubov@yandex.ru>
2023-04-12 21:14:53 +0300

    Sprint 10 #1

---
+++ setupProject/Auth/AuthViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AuthViewController.swift
+//  setupProject
+//
+//  Created by Иван Иванов on 12.04.2023.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    private let ShowWebViewSegueIdentifier = "ShowWebView"
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == ShowWebViewSegueIdentifier {
+            guard
+                let webViewViewController = segue.destination as? WebViewViewController
+            else { fatalError("Failed to prepare for \(ShowWebViewSegueIdentifier)") }
+            webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+}
+
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        //TODO: process code
+    }
+
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        dismiss(animated: true)
+    }
+}
